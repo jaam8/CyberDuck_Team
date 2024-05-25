bce7fe1c527e4156c6170d3a67a246061e559749
Kuborsky Pavel <p.kuborsky@icloud.com>
2024-05-26 03:35:59 +0800

    change models.py

--- backend/person_data/models.py
+++ backend/person_data/models.py
@@ -20,6 +20,27 @@ class Snils(models.Model):
         return self.number_of_snils
 
 
+class Organization(models.Model):
+    inn = models.CharField(max_length=10, verbose_name='ИНН')
+    ogrn = models.CharField(max_length=13, verbose_name='ОГРН')
+    kpp = models.CharField(max_length=9, verbose_name='КПП')
+    full_name = models.CharField(verbose_name='Полное наименование')
+    legal_address = models.CharField(verbose_name='Юридический адрес')
+    mailing_address = models.CharField(verbose_name='Почтовый адрес')
+
+    def __str__(self):
+        return self.full_name
+
+
+class NotarisedOfAttorney(models.Model):
+    name = models.CharField(verbose_name='ФИО')
+    inn = models.CharField(max_length=10, verbose_name='ИНН')
+    expiration_date = models.DateField(verbose_name='Дата окончания действия доверенности')
+
+    def __str__(self):
+        return self.name
+
+
 class PersonData(models.Model):
     objects = models.Manager()
     last_name = models.CharField(max_length=100, verbose_name='Фамилия')
@@ -28,6 +49,8 @@ class PersonData(models.Model):
     phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
     passport_data = models.OneToOneField(PassportData, models.CASCADE, verbose_name='Паспортные данные')
     snils = models.OneToOneField(Snils, models.CASCADE, verbose_name='СНИЛС')
+    org_id = models.ForeignKey(Organization, models.PROTECT, null=True)
+    proxy_id = models.OneToOneField(NotarisedOfAttorney, models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.last_name
