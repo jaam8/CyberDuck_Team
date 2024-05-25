3df14c478e559847ccaf5b2157fcc049a1a500ff
Kuborsky Pavel <p.kuborsky@icloud.com>
2024-05-26 01:47:36 +0800

    edit admin.py

--- backend/person_data/admin.py
+++ backend/person_data/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import PersonData
+from .models import PersonData, PassportData, Snils
 
 
 @admin.register(PersonData)
 class PersonDataAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'phone_number']
+
+
+admin.site.register(PassportData)
+admin.site.register(Snils)
