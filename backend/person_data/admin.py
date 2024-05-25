bec3a518ef368cfee17084edae5c292e59424296
Kuborsky Pavel <p.kuborsky@icloud.com>
2024-05-26 00:39:56 +0800

    admin.register(PersonData)

--- backend/person_data/admin.py
+++ backend/person_data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PersonData
 
-# Register your models here.
+
+@admin.register(PersonData)
+class PersonDataAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'phone_number']
