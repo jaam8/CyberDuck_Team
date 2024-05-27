febee74615ac8478859ef8ba1753f10f83fce662
Kuborsky Pavel <p.kuborsky@icloud.com>
2024-05-27 16:32:38 +0800

    change views.py

--- backend/person_data/views.py
+++ backend/person_data/views.py
@@ -87,23 +87,23 @@ def physical_face(request):
         passport_data = forms.PassportDataForm(request.POST)
         snils_data = forms.SnilsForm(request.POST)
         person_data = forms.PersonDataForm(request.POST)
+        organization_data = forms.OrganizationForm(request.POST)
+        notarised_of_attorney_data = forms.NotarisedOfAttorneyForm(request.POST)
 
-        if person_data.is_valid() and passport_data.is_valid() and snils_data.is_valid():
+        if person_data.is_valid() and passport_data.is_valid() and snils_data.is_valid() \
+                and organization_data.is_valid() and notarised_of_attorney_data.is_valid():
             passport_data = passport_data.save()
             snils_data = snils_data.save()
             person_data = person_data.save(commit=False)
             person_data.passport_data = passport_data
             person_data.snils = snils_data
+            organization_data = organization_data.save()
+            notarised_of_attorney_data = notarised_of_attorney_data.save()
 
-            organization_data = request.session.get('organization_data')
-            notarised_of_attorney_data = request.session.get('notarised_of_attorney_data')
             person_data.organization = organization_data
             person_data.notarised_of_attorney = notarised_of_attorney_data
 
             person_data.save()
-
-            request.session.pop('organization_data', None)
-            request.session.pop('notarised_of_attorney_data', None)
 
             return redirect('success', permanent=True)
         else:
@@ -112,11 +112,15 @@ def physical_face(request):
     person_data = forms.PersonDataForm()
     passport_data = forms.PassportDataForm()
     snils_data = forms.SnilsForm()
+    organization_data = forms.OrganizationForm()
+    notarised_of_attorney_data = forms.NotarisedOfAttorneyForm()
 
     context = {
         'person_data': person_data,
         'passport_data': passport_data,
         'snils_data': snils_data,
+        'organization': organization_data,
+        'notarised_of_attorney': notarised_of_attorney_data,
     }
     return render(request, 'physical_face.html', context)
 
@@ -126,23 +130,23 @@ def legal_face(request):
         passport_data = forms.PassportDataForm(request.POST)
         snils_data = forms.SnilsForm(request.POST)
         person_data = forms.PersonDataForm(request.POST)
+        organization_data = forms.OrganizationForm(request.POST)
+        notarised_of_attorney_data = forms.NotarisedOfAttorneyForm(request.POST)
 
-        if person_data.is_valid() and passport_data.is_valid() and snils_data.is_valid():
+        if person_data.is_valid() and passport_data.is_valid() and snils_data.is_valid() \
+                and organization_data.is_valid() and notarised_of_attorney_data.is_valid():
             passport_data = passport_data.save()
             snils_data = snils_data.save()
             person_data = person_data.save(commit=False)
             person_data.passport_data = passport_data
             person_data.snils = snils_data
+            organization_data = organization_data.save()
+            notarised_of_attorney_data = notarised_of_attorney_data.save()
 
-            organization_data = request.session.get('organization_data')
-            notarised_of_attorney_data = request.session.get('notarised_of_attorney_data')
             person_data.organization = organization_data
             person_data.notarised_of_attorney = notarised_of_attorney_data
 
             person_data.save()
-
-            request.session.pop('organization_data', None)
-            request.session.pop('notarised_of_attorney_data', None)
 
             return redirect('success', permanent=True)
         else:
@@ -151,10 +155,14 @@ def legal_face(request):
     person_data = forms.PersonDataForm()
     passport_data = forms.PassportDataForm()
     snils_data = forms.SnilsForm()
+    organization_data = forms.OrganizationForm()
+    notarised_of_attorney_data = forms.NotarisedOfAttorneyForm()
 
     context = {
         'person_data': person_data,
         'passport_data': passport_data,
         'snils_data': snils_data,
+        'organization': organization_data,
+        'notarised_of_attorney': notarised_of_attorney_data,
     }
     return render(request, 'legal_face.html', context)
